3dd2570221265440174d32c7a2267f8742d482e1
Lukas Meinhart <100217325+Drako222@users.noreply.github.com>
2022-05-25 18:24:18 +0200

    Deleted unnecessery comment

--- index.js
+++ index.js
@@ -26,30 +26,3 @@ const textblock = `###############################
 ###############################`;
 
 console.log(chalk.hex(color)(textblock));
-
-/* import emoji from 'node-emoji';
-
-// const index = Math.floor(Math.random()) * 10 * emojis.length;
-
-console.log(emoji.get(process.argv[2]));
-
-if (process.argv.length > 2 && emoji.hasEmoji(process.argv[2])) {
-  console.log(emoji.get(process.argv[2]));
-} else if(process.argv.length > 2 && !emoji.hasEmoji(process.argv[2])) {
-  console.log("Invalid emoji");
-} else {
-  console.log(emoji.random().emoji));
-}
-
-if (process.argv.length < 3) {
-  console.log(emoji.random().emoji)
-} else if (emoji.hasEmoji(process.argv[2]){
-  console.log(emoji.get(process.argv[2]));
-} else {
-  console.log("Invalid emoji");
-}
-
-} else ) {
-} else {
-  );
-} */
